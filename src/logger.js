78aeb9e0564da1fbdb111130d5f39e6a74104824
sarthak <sarthakn@gmail.com>
2018-04-15 19:04:42 -0400

    Added a simple logger that just logs message to console

---
+++ src/logger.js
@@ -0,0 +1,15 @@
+export function info(message) {
+  console.log(message);
+}
+
+export function warning(message) {
+  console.log(message);
+}
+
+export function error(message) {
+  console.log(message);
+}
+
+export function fine(message) {
+  console.log(message);
+}
